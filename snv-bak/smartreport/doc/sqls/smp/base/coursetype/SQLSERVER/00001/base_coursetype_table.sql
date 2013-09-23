b640aea493ca12b7e19e84e5489910affb15035c
Allen <hunering@gmail.com>
2013-09-24 00:34:34 +0800

    another bak

--- snv-bak/smartreport/doc/sqls/smp/base/coursetype/SQLSERVER/00001/base_coursetype_table.sql
+++
@@ -1,9 +0,0 @@
-/*课程类别*/
-create table smp_coursetype (
-/*课程类别主键*/  pk_coursetype char(20) NOT NULL,
-/*课程类别名称*/  coursetype_name varchar(50) NULL,
-CONSTRAINT PK_SMP_COURSETYPE PRIMARY KEY (pk_coursetype),
-ts char(19) null default convert(char(19),getdate(),20),
- dr smallint null default 0,
-)
- go
